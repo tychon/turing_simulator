4d178b7c96efbfd634a0667724316b229e012570
tychon <tychon4.2@googlemail.com>
2012-04-12 09:37:18 +0300

    fix small bug

--- universal.js
+++ universal.js
@@ -13,7 +13,6 @@ function arrayToString(array) {
 }
 function arrayEqu(a1, a2) {
   if (a1.length != a2.length) return false
-  for (var i = 0; i < a1.length; a1 ++) if (a1[i] != a2[i]) return false
+  for (var i = 0; i < a1.length; i ++) if (a1[i] != a2[i]) return false
   return true
 }
-
